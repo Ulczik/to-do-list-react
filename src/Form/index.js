ebd9c30365fe93e303ba80e2a1736326690cc17f
Urszula Majcher <ulunka.m@gmail.com>
2024-06-12 16:06:22 +0200

    moved to react without interaction yet

---
+++ src/Form/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Form = () => (
+  <form className="form">
+    <input
+      className="form__imput"
+      placeholder="Co jest do zrobienia"
+      autoFocus
+    />
+    <button className="form__button">Dodaj zadanie</button>
+  </form>
+);
+
+export default Form;
